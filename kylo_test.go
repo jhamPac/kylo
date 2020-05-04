73befdb638faeba3a0239fa3fb5e7038dc608158
jhamPac <cloud@kevia.me>
2020-05-03 20:17:32 -0700

    simple test for a reader

--- kylo_test.go
+++ kylo_test.go
@@ -1,9 +1,36 @@
 package kylo
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestContextAwareReader(t *testing.T) {
 	t.Run("normal reader test", func(t *testing.T) {
+		rdr := strings.NewReader("abcdefghijk")
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
 
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "abc")
+
+		_, err := rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "def")
 	})
+}
+
+func assertBufferHas(t *testing.T, buf []byte, want string) {
+	t.Helper()
+	got := string(buf)
+	if got != want {
+		t.Errorf("got %q, expected %q", got, want)
+	}
 }
