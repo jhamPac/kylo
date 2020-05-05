971252faf4b578bf8b5c34cdec64f870d7fabc1c
jhamPac <cloud@kevia.me>
2020-05-04 19:24:21 -0700

    ctx reader

--- kylo.go
+++ kylo.go
@@ -5,7 +5,22 @@ import (
 	"io"
 )
 
+type readerCtx struct {
+	ctx      context.Context
+	delegate io.Reader
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.delegate.Read(p)
+}
+
 // NewCancellableReader takes in rdr and returns an io.Reader
 func NewCancellableReader(ctx context.Context, rdr io.Reader) io.Reader {
-	return rdr
+	return &readerCtx{
+		ctx:      ctx,
+		delegate: rdr,
+	}
 }
